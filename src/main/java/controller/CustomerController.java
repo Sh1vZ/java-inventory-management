68c9dcd59bb1e9fb10c7e3a69c5153bd9967edab
sh1vz <shivam.bhagwandas@gmail.com>
2024-03-12 22:55:37 -0300

    refactor: add dto

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -1,16 +1,17 @@
 package controller;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import dto.customer.CustomerDTO;
 import entity.Customer;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import mapper.Mapper;
 import service.CustomerService;
 import util.BodyValidationUtil;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 
@@ -24,43 +25,43 @@ public class CustomerController extends BaseController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Customer> findAll() {
-        return this.customerService.findAllCustomers();
+    public Response findAll() {
+        return buildResponse("ok", Response.Status.OK, Mapper.mapList(this.customerService.findAllCustomers(), CustomerDTO.class));
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public Response createCustomer(@Valid Customer customer) {
-        Response validationResponse = BodyValidationUtil.validateAndBuildResponse(customer);
+    public Response createCustomer(@Valid CustomerDTO customerDto) {
+        Response validationResponse = BodyValidationUtil.validateAndBuildResponse(customerDto);
         if (validationResponse != null) {
             return validationResponse;
         }
-
+        Customer customer = Mapper.map(customerDto, Customer.class);
         customer = customerService.createCustomer(customer);
-        return buildResponse("created", Response.Status.CREATED, customer);
+        customerDto = Mapper.map(customer, CustomerDTO.class);
+        return buildResponse("created", Response.Status.CREATED, customerDto);
     }
 
     @PUT
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateCustomer(@PathParam("id") Long id, @Valid Customer customer) {
+    public Response updateCustomer(@PathParam("id") Long id, @Valid CustomerDTO customerDto) {
         Customer existingCustomer = customerService.getCustomerById(id);
         if (existingCustomer == null) {
             Map<String, String> jsonResponse = Collections.singletonMap("error", "Customer not found");
             return buildResponse("not-found", Response.Status.NOT_FOUND, jsonResponse);
         }
-
-        existingCustomer.setName(customer.getName());
-
-        Response validationResponse = BodyValidationUtil.validateAndBuildResponse(existingCustomer);
+        Response validationResponse = BodyValidationUtil.validateAndBuildResponse(customerDto);
         if (validationResponse != null) {
             return validationResponse;
         }
-        existingCustomer = customerService.updateCustomer(existingCustomer);
-        return buildResponse("updated", Response.Status.OK, existingCustomer);
+
+        existingCustomer.setName(customerDto.getName());
+        customerDto = Mapper.map(customerService.updateCustomer(existingCustomer), CustomerDTO.class);
+        return buildResponse("updated", Response.Status.OK, customerDto);
     }
 
     @GET
@@ -72,8 +73,7 @@ public class CustomerController extends BaseController {
             return buildResponse("customer-not-found", Response.Status.NOT_FOUND, null);
         }
 
-        customerService.updateCustomer(existingCustomer);
-        return buildResponse("ok", Response.Status.OK, existingCustomer);
+        return buildResponse("ok", Response.Status.OK, Mapper.map(customerService.updateCustomer(existingCustomer), CustomerDTO.class));
     }
 
     @DELETE
@@ -86,7 +86,7 @@ public class CustomerController extends BaseController {
 
         }
         existingCustomer = customerService.deleteCustomer(id);
-        return buildResponse("ok", Response.Status.OK, existingCustomer);
+        return buildResponse("ok", Response.Status.OK, Mapper.map(existingCustomer, CustomerDTO.class));
     }
 }
 
---
+++ src/main/java/dto/customer/CustomerDTO.java
@@ -0,0 +1,54 @@
+package dto.customer;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Date;
+
+public class CustomerDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
