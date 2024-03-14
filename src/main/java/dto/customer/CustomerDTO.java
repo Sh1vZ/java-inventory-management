da3e111f3f87efa267cac65edfc15c0d6d028b19
sh1vz <shivam.bhagwandas@gmail.com>
2024-03-14 00:37:51 -0300

    feat: add transaction to customer dto

--- src/main/java/dto/customer/CustomerDTO.java
+++ src/main/java/dto/customer/CustomerDTO.java
@@ -1,9 +1,11 @@
 package dto.customer;
 
+import dto.transaction.CustomerTransactionDTO;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
+import java.util.List;
 
 public class CustomerDTO {
 
@@ -11,13 +13,20 @@ public class CustomerDTO {
 
     @NotBlank(message = "Name must not be blank")
     private String name;
-
+    private List<CustomerTransactionDTO> transactions;
     @Column(name = "created_at", nullable = false, updatable = false)
     private Date createdAt;
 
     @Column(name = "updated_at")
     private Date updatedAt;
 
+    public List<CustomerTransactionDTO> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<CustomerTransactionDTO> transactions) {
+        this.transactions = transactions;
+    }
 
     public Long getId() {
         return id;
---
+++ src/main/java/dto/transaction/CustomerTransactionDTO.java
@@ -0,0 +1,46 @@
+package dto.transaction;
+
+import java.util.Date;
+
+public class CustomerTransactionDTO {
+    private Long id;
+
+    private Long total;
+
+    private Date createdAt;
+
+
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
