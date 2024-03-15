79b8b809f9e5cafa6fc046b456a461ca051f465a
sh1vz <shivam.bhagwandas@gmail.com>
2024-03-14 21:07:10 -0300

    feat: add cors to response

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -11,7 +11,9 @@ public class BaseController {
         Map<String, Object> jsonResponse = new HashMap<>();
         jsonResponse.put("message", message);
         jsonResponse.put("data", data);
-        return Response.status(status)
+        return Response.status(status).header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With")
                 .entity(jsonResponse)
                 .type(MediaType.APPLICATION_JSON)
                 .build();
